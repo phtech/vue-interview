80ec3edb25587c7e2a1e188f3f863cf4650b4c13
Travis <travis.n.rea@gmail.com>
2020-09-01 16:33:03 -0700

    about me

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an page about ME!</h1>
+    <img src='../assets/Avatar.png'/>
+    <h3>Activities:</h3>
+    <p>Hello I am Austen, ouside programing my passion is the outdoors and I love climbing!</p>
+    <h3>Why I like Computers:</h3>
+    <p>They are fasinating to learn about the way they compute.</p>
+  </div>
+</template>
+<style scoped>
+img {
+  height: 250px;
+  width: 200px;
+  border-radius: 5px;
+}
+p {
+  text-align: left;
+  padding-left: 10%;
+}
+h3 {
+  text-align: left;
+  padding-left: 5%
+}
+</style>
