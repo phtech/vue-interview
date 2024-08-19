27c8c86e3d8c9f24faefeff47d75319c13cf20bd
Andy <andy.herrmann@phtech.com>
2024-08-19 12:07:18 -0700

    Fix unit tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,8 +5,9 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { incomingMsg: msg }
     })
+    expect(wrapper.vm.msg).toMatch(msg)
     expect(wrapper.text()).toMatch(msg)
   })
 })
